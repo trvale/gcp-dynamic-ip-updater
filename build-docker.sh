494ddc3cef40cace000df71fd1e123070c02340e
Justin Travaille <1981652+trvale@users.noreply.github.com>
2025-03-29 22:13:30 -0700

    changing image build process

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,3 @@
 docker build -t gcp-dynamic-ip-updater .
 echo "Copying docker image to containerd"
-docker save gcp-dynamic-ip-updater:latest -o /tmp/gcp-dynamic-ip-updater.tar
-ctr image import /tmp/gcp-dynamic-ip-updater.tar
-rm -f /tmp/gcp-dynamic-ip-updater.tar
+docker save gcp-dynamic-ip-updater:latest | sudo k3s ctr images import -
