10a941910f53e4226f79da15ae5e09272227cf78
Justin Travaille <1981652+trvale@users.noreply.github.com>
2025-03-30 21:15:33 -0700

    Removing sudo on build-docker script

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 docker build -t gcp-dynamic-ip-updater .
 echo "Copying docker image to containerd"
-docker save gcp-dynamic-ip-updater:latest | sudo k3s ctr images import -
+docker save gcp-dynamic-ip-updater:latest | k3s ctr images import -
